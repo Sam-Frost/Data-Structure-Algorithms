6068b2b4c8bf3851299378d88f5c7a90f5b28b75
Sam Frost <sambrofrost@gmail.com>
2025-04-07 23:33:43 +0530

    Swapping two integer

---
+++ twoIntegerSwap.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class SwapExample {
+
+    // Method to swap two elements in an array
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+
+}
