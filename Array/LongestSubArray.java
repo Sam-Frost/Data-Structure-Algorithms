51771d333d672d1847963ba2d37322bb03474a80
Sam Frost <sambrofrost@gmail.com>
2025-04-09 23:02:12 +0530

    Added longeset sub array

---
+++ Array/LongestSubArray.java
@@ -0,0 +1,69 @@
+package Array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubArray {
+
+    // Best Solution if array has +ve, 0 and -ve
+    public int longestSubArray(int[] arr, int k) {
+
+        Map<Integer, Integer> preSumMap = new HashMap<>();
+
+        int maxLength = 0;
+        int sum = 0;
+
+        for(int i = 0; i < arr.length; i++){
+
+            sum += arr[i];
+
+
+            if(sum == k) {
+                maxLength =   Math.max(maxLength, i+1);
+            }
+
+            int remain = sum - k;
+
+            Integer val = preSumMap.get(remain);
+
+            if(val != null) {
+                maxLength =   Math.max(maxLength,i - val);
+            }
+
+            if(preSumMap.get(sum) == null)
+                preSumMap.put(sum, i);
+
+        }
+
+        return maxLength;
+
+    }
+
+    // Best Solution if array has only +ve
+    public int longestSubArrayPositive(int[] arr, int k) {
+
+        int maxLength = 0;
+        int sum = arr[0];
+        int low = 0;
+        int high = 0;
+
+        while(high < arr.length) {
+            while(sum > k) {
+                sum -= arr[low];
+                low--;
+            }
+
+            high++;
+            if(high < arr.length)
+                sum =+ arr[high];
+
+            if(sum == k){
+                maxLength = Math.max(maxLength, high - low + 1);
+            }
+
+        }
+
+        return maxLength;
+
+    }
+}
