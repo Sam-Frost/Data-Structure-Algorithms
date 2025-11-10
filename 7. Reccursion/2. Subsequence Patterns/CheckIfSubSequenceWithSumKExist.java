bd50ba05dc6cdb7e01ac96fb97c8bbdeb736a0fb
Sam Frost <sambrofrost@gmail.com>
2025-11-10 17:26:37 +0530

    question : check if sub sequence with sum k exist

---
+++ 7. Reccursion/2. Subsequence Patterns/CheckIfSubSequenceWithSumKExist.java
@@ -0,0 +1,26 @@
+
+public class CheckIfSubSequenceWithSumKExist {
+
+    public boolean checkSubsequenceSum(int[] nums, int k) {
+        return generate(0, 0, nums, k);
+    }
+
+    public boolean generate(int index, int currSum, int[] nums, int k) {
+        if (k == 0) {
+            return true;
+        }
+
+        if (k < 0) {
+            return false;
+        }
+
+        if (index == nums.length) {
+            return false;
+        }
+
+        boolean with = generate(index + 1, currSum + nums[index], nums, k - nums[index]);
+        boolean without = generate(index + 1, currSum, nums, k);
+
+        return with || without;
+    }
+}
