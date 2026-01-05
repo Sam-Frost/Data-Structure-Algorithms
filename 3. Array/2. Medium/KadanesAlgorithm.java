d35837d3c1a75a4ce50e3eb3c7ab578b59a822fa
Sam Frost <sambrofrost@gmail.com>
2026-01-04 21:31:43 -0500

    question : maximum sub array sum( kadane's algorithm)

---
+++ 3. Array/2. Medium/KadanesAlgorithm.java
@@ -0,0 +1,24 @@
+/*
+ Time Complexity: O(n)
+ Space Complexity: O(1)
+ */
+public class KadanesAlgorithm {
+
+    public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (currSum < 0) {
+                currSum = nums[i];
+            } else {
+                currSum += nums[i];
+            }
+
+            maxSum = Math.max(maxSum, currSum);
+        }
+
+        return maxSum;
+    }
+}
